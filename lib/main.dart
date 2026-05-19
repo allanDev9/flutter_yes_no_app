60af2b9d89996456d48b7439580a9815210b2694
allanDev9 <allanpenaguerra2003@gmail.com>
2026-05-19 13:24:48 -0600

    Update main app text to 'Yes No App!'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello Worlddddd!'),
+          child: Text('Yes No App!'),
         ),
       ),
     );
